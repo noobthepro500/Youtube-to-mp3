713a3f84488a7c43b5eaa4dd51361fede25b24d8
Froxzen <harvardchong1@gmail.com>
2024-10-25 06:40:03 +1100

    Refactor app.js to set custom views directory using path.join

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const fetch = require("node-fetch");
 require("dotenv").config();
+const path = require('path'); // Make sure to import path
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+// Set the view engine and views directory
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); // Set views directory relative to the current file
 app.use(express.static("public"));
 
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.render("index");
-})
+});
 
 app.post("/convert-mp3", async (req, res) => {
-
     const videoId = req.body.videoId;
-    
-    if(videoId === undefined || videoId === "" || videoId === null) {
-        return res.render("index", { success : false, message : "Please enter a video ID"});
+
+    if (videoId === undefined || videoId === "" || videoId === null) {
+        return res.render("index", { success: false, message: "Please enter a video ID" });
     } else {
         const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
             "method": "GET",
@@ -33,21 +32,21 @@ app.post("/convert-mp3", async (req, res) => {
                 "x-rapidapi-host": process.env.API_HOST
             }
         });
-  
+
         const fetchResponse = await fetchAPI.json();
-    
-        if(fetchResponse.status === "ok")
-            return res.render("index",{ 
-            success : true,  
-            song_title : fetchResponse.title, 
-            song_link : fetchResponse.link
-        })
-        else {
-            return res.render("index", { success : false, message : fetchResponse.msg});
+
+        if (fetchResponse.status === "ok") {
+            return res.render("index", {
+                success: true,
+                song_title: fetchResponse.title,
+                song_link: fetchResponse.link
+            });
+        } else {
+            return res.render("index", { success: false, message: fetchResponse.msg });
         }
     }
 });
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-})
+});
