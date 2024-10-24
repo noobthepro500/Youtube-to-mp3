c0f243eb7cdb4c2ce69b33e34cf625aeeb0779d6
Froxzen <harvardchong1@gmail.com>
2024-10-25 00:50:59 +1100

    Refactor app.js to add body parsing middleware and root route handler

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const PORT = process.env.PORT || 3000
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
