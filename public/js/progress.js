9e95bc978ac069a9bec698816e5fa5a92a02fd7e
Froxzen <harvardchong1@gmail.com>
2024-10-25 03:27:43 +1100

    Removed comments

--- public/js/progress.js
+++ public/js/progress.js
@@ -4,13 +4,11 @@ document.getElementById('form').addEventListener('submit', function(event) {
     const progressBar = document.getElementById('progress-bar');
     progressBar.style.width = '0%';
     
-    // Simulate progress
     setTimeout(() => {
         progressBar.style.width = '100%';
-    }, 100); // Start the animation after a short delay
+    }, 100); 
   
-    // Submit the form after the progress bar reaches 100%
     setTimeout(() => {
         this.submit();
-    }, 600); // Adjust this timeout to match the transition duration
+    }, 600); 
 });
