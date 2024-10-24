031c39561cf9b49fb05fdac8256b731f0d1e5926
Froxzen <harvardchong1@gmail.com>
2024-10-25 07:10:34 +1100

    Refactor app.js to set correct path for static files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Set the view engine and views directory
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views')); // Set views directory relative to the current file
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public'))); // Ensure correct path to public directory
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
