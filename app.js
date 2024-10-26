281b4304d3c6a93145511532629b12d2d90a649b
Froxzen <harvardchong1@gmail.com>
2024-10-26 19:30:20 +1100

    Added path.join for vercel

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
