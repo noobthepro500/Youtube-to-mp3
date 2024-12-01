d3af5a71e9f702bf5e8de0a66ace4ba5c9bdc6d1
Froxzen <harvardchong1@gmail.com>
2024-12-01 23:50:21 +1100

    Reverted import changes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const fetch = require("node-fetch");
-require("dotenv").config();
+import express from "express";
+import fetch from "node-fetch";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-const path = require("path");
-
 app.set("views", path.join(__dirname, "views"));
+
 app.set("public", path.join(__dirname, "public"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
