b12dda8869de116cf70b69edeb4ab7540858491e
Froxzen <harvardchong1@gmail.com>
2024-12-02 00:19:59 +1100

    Made default theme dark-mode

--- public/light-dark-mode.js
+++ public/light-dark-mode.js
@@ -1,25 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const themeToggle = document.getElementById('theme-toggle');
-  
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    themeToggle.checked = true;
-  }
+document.addEventListener("DOMContentLoaded", () => {
+	const themeToggle = document.getElementById("theme-toggle");
 
-  themeToggle.addEventListener('change', function() {
-    if (this.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.documentElement.removeAttribute('data-theme');
-      localStorage.setItem('theme', 'light');
-    }
-  });
+	const savedTheme = localStorage.getItem("theme");
+	if (savedTheme === "dark" || !savedTheme) {
+		document.documentElement.setAttribute("data-theme", "dark");
+		themeToggle.checked = true;
+	} else {
+		document.documentElement.setAttribute("data-theme", "light");
+		themeToggle.checked = false;
+	}
 
-  document.body.style.transition = 'none';
-  document.documentElement.classList.add('theme-loaded');
-  setTimeout(() => {
-    document.body.style.transition = '';
-  }, 1);
+	themeToggle.addEventListener("change", function () {
+		if (this.checked) {
+			document.documentElement.setAttribute("data-theme", "dark");
+			localStorage.setItem("theme", "dark");
+		} else {
+			document.documentElement.setAttribute("data-theme", "light");
+			localStorage.setItem("theme", "light");
+		}
+	});
+
+	document.body.style.transition = "none";
+	document.documentElement.classList.add("theme-loaded");
+	setTimeout(() => {
+		document.body.style.transition = "";
+	}, 1);
 });
